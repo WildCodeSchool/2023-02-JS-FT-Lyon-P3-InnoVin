2e833a407e8df5d229a6cda53e6f2b70af0862b6
VanessaGrd <suede210810@gmail.com>
2023-07-12 21:17:55 +0200

    tracks_backgroundcolor

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,8 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import "./Recipe.css";
+// import { Box, Typography } from "@mui/material";
 import React, { useState } from "react";
 import ReactSlider from "react-slider";
-import logo from "../assets/logo.svg";
-import styles from "./Recipe.module.css";
+// import logo from "../assets/logo.svg";
 import winebottle from "../assets/winebottle.svg";
 
 export default function Recipe() {
@@ -11,9 +11,10 @@ export default function Recipe() {
   const totalWine3 = valueWine[0] - 0;
   const totalWine2 = valueWine[1] - valueWine[0];
   const totalWine1 = 250 - valueWine[1];
+
   return (
     <>
-      <Box flexDirection="row" display="flex" marginBottom="2rem">
+      {/* <Box flexDirection="row" display="flex" marginBottom="2rem">
         <img src={logo} alt="logo" />
         <Typography
           sx={{
@@ -23,6 +24,7 @@ export default function Recipe() {
             width: "56vw",
             color: "secondary.main",
             fontSize: "calc(2.5rem + 1vmin)",
+            fontFamily: "EB Garamond",
           }}
         >
           {" "}
@@ -43,27 +45,31 @@ export default function Recipe() {
           {" "}
           Veuillez rentrer les dosages{" "}
         </Typography>
-      </Box>
-      <div className={styles.pageContainer}>
-        <img className={styles.wineBottle} src={winebottle} alt="winebottle" />
-
-        <ReactSlider
-          className={styles.verticalSlider}
-          thumbClassName="example-thumb"
-          trackClassName={styles.exampleTrack}
-          defaultValue={[50, 100]}
-          max={250}
-          min={0}
-          ariaLabel={["Lowest thumb", "Middle thumb", "Top thumb"]}
-          /* eslint-disable-next-line react/jsx-props-no-spreading */
-          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-          orientation="vertical"
-          invert
-          pearling
-          minDistance={10}
-          onChange={(value) => setValueWine(value)}
-        />
-
+        </Box> */}
+      <div className="pageContainer">
+        <div className="wineBottle">
+          <img src={winebottle} alt="winebottle" />
+        </div>
+        <div className="sliderContainer">
+          <ReactSlider
+            className="vertical-slider"
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            defaultValue={[50, 100]}
+            max={250}
+            min={0}
+            ariaLabel={["Lowest thumb", "Top thumb"]}
+            renderThumb={(props, state) => (
+              /* eslint-disable-next-line react/jsx-props-no-spreading */
+              <div {...props}>{state.valueNow}</div>
+            )}
+            orientation="vertical"
+            invert
+            pearling
+            minDistance={10}
+            onChange={(value) => setValueWine(value)}
+          />
+        </div>
         <p>Vin 1 : {totalWine1}</p>
         <p>Vin 2 : {totalWine2}</p>
         <p>Vin 3 : {totalWine3}</p>
