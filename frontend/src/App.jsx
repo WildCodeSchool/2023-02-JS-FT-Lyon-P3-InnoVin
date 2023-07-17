ffc3de27fdc3b0dad2dd7ae1b221c69767fba526
Guillaume Lamourec <parkerpool@live.fr>
2023-07-17 11:33:20 +0200

    protected front routes to only allow connected users to access tasting workshop, and connected admin to acces admin part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import TastingSheet from "./pages/TastingSheet";
 import TastingProfile from "./pages/TastingProfile";
 import Recipe from "./pages/Recipe";
+import RequiredAuth from "./components/RequiredAuth";
 
 function App() {
   return (
@@ -18,17 +19,22 @@ function App() {
         <Route path="/" element={<Presentation />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Inscription />} />
-        <Route path="/tasting" element={<Outlet />}>
+        <Route
+          path="/tasting"
+          element={<RequiredAuth allowedRoles={["Admin", "Utilisateur"]} />}
+        >
           <Route index element={<Tasting />} />
           <Route path="tastingsheet" element={<TastingSheet />} />
           <Route path="tastingprofile" element={<TastingProfile />} />
           <Route path="recipe" element={<Recipe />} />
         </Route>
-        <Route path="/admin/home" element={<Admin />} />
-        <Route path="/admin/users" element={<Admin />} />
-        <Route path="/admin/wines" element={<Admin />} />
-        <Route path="/admin/sessions" element={<Admin />} />
-        <Route path="/admin/recipes" element={<Admin />} />
+        <Route element={<RequiredAuth allowedRoles={["Admin"]} />}>
+          <Route path="/admin/home" element={<Admin />} />
+          <Route path="/admin/users" element={<Admin />} />
+          <Route path="/admin/wines" element={<Admin />} />
+          <Route path="/admin/sessions" element={<Admin />} />
+          <Route path="/admin/recipes" element={<Admin />} />
+        </Route>
       </Routes>
       <ToastContainer />
     </div>
