9c0dd59f4900d17133c2e59282aa73a1b26deac9
Guillaume Lamourec <parkerpool@live.fr>
2023-07-26 12:01:12 +0200

    handle jwt token expiration

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import history from "./history";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 403) {
+      history.replace(`/login?expired`);
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
